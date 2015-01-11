61b287c6f0c9ec92ef7f8f3d696b07574a597f09
Maximilian Maroe <drakfyre@gmail.com>
2015-01-10 19:51:48 -0800

    Penalty display

---
+++ Assets/Scripts/DisplayScorePenalty.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayScorePenalty : MonoBehaviour
+{
+	private Text text;
+	private string initialText;
+	
+	void Start()
+	{
+		text = GetComponent<Text>();
+		initialText = text.text;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+		ScoreInfo scoreInfo = CollisionBug.EvaluateScore();
+		text.text = initialText + scoreInfo.penalty;
+	}
+}
