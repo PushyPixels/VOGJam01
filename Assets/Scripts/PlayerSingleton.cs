c80b8b8c3b409b1242c88e203efd135d355b5989
Maximilian Maroe <drakfyre@gmail.com>
2015-01-10 21:35:52 -0800

    Player singleton (persist player between levels)

---
+++ Assets/Scripts/PlayerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSingleton : MonoBehaviour
+{
+	public static PlayerSingleton Instance;
+
+	// Use this for initialization
+	void Awake()
+	{
+		if(Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Instance.transform.position = transform.position;
+			Instance.transform.rotation = transform.rotation;
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+}
