462094ab5c6c40f3ba3425d2932f3fc7633363ff
Maximilian Maroe <drakfyre@gmail.com>
2015-01-10 22:08:41 -0800

    Cool power up bro

---
+++ Assets/Scripts/RotateAroundAxis.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundAxis : MonoBehaviour
+{
+	public Vector3 axis = Vector3.forward;
+	public float rotationSpeed = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate(axis,rotationSpeed*Time.deltaTime);
+	}
+}
